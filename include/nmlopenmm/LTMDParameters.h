f67e80e73b077add13ab791aa80e519b785451fc
Trevor Cickovski <movingpictures83@gmail.com>
2011-11-18 16:36:59 +0000

    New structs

---
+++ include/nmlopenmm/LTMDParameters.h
@@ -0,0 +1,24 @@
+#ifndef LTMDPARAMETERS_H
+#define LTMDPARAMETERS_H
+
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+struct LTMDForce {
+   LTMDForce(string n, int i) : name(n), index(i) {}
+   string name;
+   int index;
+};
+
+struct LTMDParameters {
+    double delta;
+    vector<int> residue_sizes;
+    int res_per_block;
+    int bdof;
+    vector<LTMDForce> forces;
+};
+
+
+#endif
