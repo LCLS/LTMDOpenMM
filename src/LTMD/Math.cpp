b131d860cab7a8f1144b87670b3ab5b6f13603b8
James Sweet <james.sweet88@googlemail.com>
2012-06-06 16:21:51 +0000

    Removed mkl for diagonalization to allow for using openmp

--- src/LTMD/Math.cpp
+++ src/LTMD/Math.cpp
@@ -57,62 +57,7 @@ void MatrixMultiply( const TNT::Array2D<double>& matrixA, const TNT::Array2D<dou
  * We assume input matrix is square and symmetric.
  */
 void FindEigenvalues( const TNT::Array2D<double>& matrix, TNT::Array1D<double>& values, TNT::Array2D<double>& vectors ) {
-#ifdef INTEL_MKL
-	const int dim = matrix.dim1();
-
-	const char cmach = 'S';
-	const double abstol = dlamch( &cmach ); 
-	
-	const char jobz = 'V'; // compute eigenvectors and eigenvalues
-	const char range = 'A'; // compute all eigenvalues
-	const char uplo = 'U'; // use upper triangular part
-	const int n = dim;
-	const int lda = dim;
-	const int ldz = dim;
-
-	const int lwork = 26 * n; // workspace size suggested by manual
-	const int liwork = 10 * n; //  i work size suggested by manual
-
-	// input matrices
-	std::vector<double> a(lda * n); // matrix
-	std::vector<double> work(lwork); // workspace
-	std::vector<double> iwork(liwork); // iwork workspace
-
-	// not referenced
-	double vl = 0.0;
-	double vu = 0.0;
-	double il = 0.0;
-	double iu = 0.0;
-
-	// output parameters
-	int m = n; // number eigenpairs found, should equal n
-	std::vector<double> w(m); // stores eigenvalues
-	std::vector<double> z(ldz*m); // stores eigenvectors
-	std::vector<int> isuppz(2 * m); // gives indices bounding non-zero regions of eigenvectors
-	
-	for( int i = 0; i < lda; i++ ) {
-		for( int j = 0; j < n; j++ ) {
-			a[i * lda + j] = matrix[i][j];
-		}
-	}
-	
-	double *wPointer = &w[0];
-	double *zPointer = &z[0];
-
-	int info = LAPACKE_dsyevr( LAPACK_ROW_MAJOR, jobz, range, uplo, n, &a[0], lda, vl, vu, il, iu, abstol, &m, wPointer, zPointer, ldz, &isuppz[0]);
-
-	if( wPointer != &w[0] ) std::cout << "WDiffers" << std::endl;
-	if( zPointer != &z[0] ) std::cout << "ZDiffers" << std::endl;
-	
-	for( int i = 0; i < dim; i++ ) {
-		values[i] = w[i];
-		for( int j = 0; j < dim; j++ ) {
-			vectors[i][j] = z[i * dim + j];
-		}
-	}
-#else
 	JAMA::Eigenvalue<double> decomp( matrix );
 	decomp.getRealEigenvalues( values );
 	decomp.getV( vectors );
-#endif
 }
