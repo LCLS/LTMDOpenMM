6687992cde26c6870d598f31056f7f7076182eae
James Sweet <james.sweet88@googlemail.com>
2012-08-21 09:45:24 -0400

    Removed custom forces and replaced with second order standard forces

--- src/LTMD/Analysis.cpp
+++ src/LTMD/Analysis.cpp
@@ -135,135 +135,49 @@ namespace OpenMM {
 			Context &context = contextImpl.getOwner();
 			State state = context.getState( State::Positions | State::Forces );
 			vector<Vec3> positions = state.getPositions();
-			
-			/*********************************************************************/
-			/*                                                                   */
-			/* Block Hessian Code (Cickovski/Sweet)                              */
-			/*                                                                   */
-			/*********************************************************************/
             
-			// Initial residue data (where in OpenMM?)
-            
-			// For now, since OpenMM input files do not contain residue information
-			// I am assuming that they will always start with the N-terminus, just for testing.
-			// This is true for the villin.xml but may not be true in the future.
-			// need it to parallelize.
-            
-			if( !mInitialized ) {
-				Initialize( context, params );
-			}
-			
 			int n = 3 * mParticleCount;
             
-			// Copy the positions.
-			vector<Vec3> blockPositions;
-			for( unsigned int i = 0; i < mParticleCount; i++ ) {
-				Vec3 atom( state.getPositions()[i][0], state.getPositions()[i][1], state.getPositions()[i][2] );
-				blockPositions.push_back( atom );
-			}
-            
-			blockContext->setPositions( blockPositions );
-			/*********************************************************************/
+            TNT::Array2D<double> h( n, n, 0.0 );
+            std::vector<Vec3> blockPositions( positions );
             
 #ifdef FIRST_ORDER
-            vector<Vec3> block_start_forces = blockContext->getState( State::Forces ).getForces();
+            vector<Vec3> forces0 = context.getState( State::Forces ).getForces();
 #endif
             
-			TNT::Array2D<double> h( n, n, 0.0 );
-			vector<Vec3> initialBlockPositions( blockPositions );
-			for( unsigned int i = 0; i < mLargestBlockSize; i++ ) {
-				// Perturb the ith degree of freedom in EACH block
-				// Note: not all blocks will have i degrees, we have to check for this
-				for( unsigned int j = 0; j < blocks.size(); j++ ) {
-					unsigned int dof_to_perturb = 3 * blocks[j] + i;
-					unsigned int atom_to_perturb = dof_to_perturb / 3;  // integer trunc
-                    
-					// Cases to not perturb, in this case just skip the block
-					if( j == blocks.size() - 1 && atom_to_perturb >= mParticleCount ) {
-						continue;
-					}
-					if( j != blocks.size() - 1 && atom_to_perturb >= blocks[j + 1] ) {
-						continue;
-					}
-                    
-					blockPositions[atom_to_perturb][dof_to_perturb % 3] = initialBlockPositions[atom_to_perturb][dof_to_perturb % 3] - params.blockDelta;
-				}
+            // Set position and calculate forces
+            for( unsigned int i = 0; i < n; i++ ){
+                // Perturb backward
+                blockPositions[i/3][i%3] = positions[i/3][i%3] - params.blockDelta;
                 
-				blockContext->setPositions( blockPositions );
-				vector<Vec3> forces1 = blockContext->getState( State::Forces ).getForces();
-                
+                context.setPositions( blockPositions );
+                vector<Vec3> forces1 = context.getState( State::Forces ).getForces();
+
 #ifndef FIRST_ORDER
-				// Now, do it again...
-				for( int j = 0; j < blocks.size(); j++ ) {
-					int dof_to_perturb = 3 * blocks[j] + i;
-					int atom_to_perturb = dof_to_perturb / 3;  // integer trunc
-                    
-					// Cases to not perturb, in this case just skip the block
-					if( j == blocks.size() - 1 && atom_to_perturb >= mParticleCount ) {
-						continue;
-					}
-					if( j != blocks.size() - 1 && atom_to_perturb >= blocks[j + 1] ) {
-						continue;
-					}
-                    
-					blockPositions[atom_to_perturb][dof_to_perturb % 3] = initialBlockPositions[atom_to_perturb][dof_to_perturb % 3] + params.blockDelta;
-				}
-                
-				blockContext->setPositions( blockPositions );
-				vector<Vec3> forces2 = blockContext->getState( State::Forces ).getForces();
+                // Perturb forward
+                blockPositions[i/3][i%3] = positions[i/3][i%3] + params.blockDelta;
+
+                context.setPositions( blockPositions );
+                vector<Vec3> forces2 = context.getState( State::Forces ).getForces();
 #endif
-                
-				// revert block positions
-				for( int j = 0; j < blocks.size(); j++ ) {
-					int dof_to_perturb = 3 * blocks[j] + i;
-					int atom_to_perturb = dof_to_perturb / 3;  // integer trunc
-                    
-					// Cases to not perturb, in this case just skip the block
-					if( j == blocks.size() - 1 && atom_to_perturb >= mParticleCount ) {
-						continue;
-					}
-					if( j != blocks.size() - 1 && atom_to_perturb >= blocks[j + 1] ) {
-						continue;
-					}
-                    
-					blockPositions[atom_to_perturb][dof_to_perturb % 3] = initialBlockPositions[atom_to_perturb][dof_to_perturb % 3];
-                    
-				}
-                
-				for( int j = 0; j < blocks.size(); j++ ) {
-					int dof_to_perturb = 3 * blocks[j] + i;
-					int atom_to_perturb = dof_to_perturb / 3;  // integer trunc
-                    
-					// Cases to not perturb, in this case just skip the block
-					if( j == blocks.size() - 1 && atom_to_perturb >= mParticleCount ) {
-						continue;
-					}
-					if( j != blocks.size() - 1 && atom_to_perturb >= blocks[j + 1] ) {
-						continue;
-					}
-                    
-					int col = dof_to_perturb;
-                    
-					int start_dof = 3 * blocks[j];
-					int end_dof;
-					if( j == blocks.size() - 1 ) {
-						end_dof = 3 * mParticleCount;
-					} else {
-						end_dof = 3 * blocks[j + 1];
-					}
-                    
-					for( int k = start_dof; k < end_dof; k++ ) {
+
+                // Revert
+                blockPositions[i/3][i%3] = positions[i/3][i%3];
+
+                // Fill matrix
+                for( int j = 0; j < n; j++ ){
 #ifdef FIRST_ORDER
-                        double blockscale = 1.0 / ( params.blockDelta * sqrt( mParticleMass[atom_to_perturb] * mParticleMass[k / 3] ) );
-                        h[k][col] = ( forces1[k / 3][k % 3] - block_start_forces[k / 3][k % 3] ) * blockscale;
+                    double blockscale = 1.0 / (params.blockDelta * sqrt(mParticleMass[i/3] * mParticleMass[j/3]));
+                    h[j][i] = (forces1[j/3][j%3] - forces0[j/3][j%3]) * blockscale;
 #else
-						double blockscale = 1.0 / ( 2 * params.blockDelta * sqrt( mParticleMass[atom_to_perturb] * mParticleMass[k / 3] ) );
-						h[k][col] = ( forces1[k / 3][k % 3] - forces2[k / 3][k % 3] ) * blockscale;
+                    double blockscale = 1.0 / (2 * params.blockDelta * sqrt(mParticleMass[i/3] * mParticleMass[j/3]));
+                    h[j][i] = (forces1[j/3][j%3] - forces2[j/3][j%3]) * blockscale;
 #endif
-					}
-				}
-			}
-            
+                }
+            }
+
+            context.setPositions(positions);
+             
 			gettimeofday( &tp_hess, NULL );
             
             const double hessElapsed = ( tp_hess.tv_sec - tp_begin.tv_sec ) * 1000.0 + ( tp_hess.tv_usec - tp_begin.tv_usec ) / 1000.0;
@@ -517,229 +431,6 @@ namespace OpenMM {
 					file << j << " " << i << " " << U[j][i] << std::endl;
 				}
 			}
-#endif
-		}
-        
-		void Analysis::Initialize( Context &context, const Parameters &params ) {
-#ifdef PROFILE_ANALYSIS
-			timeval start, end;
-			gettimeofday( &start, 0 );
-#endif
-            
-			// Get Current System
-			System &system = context.getSystem();
-			
-			// Store Particle Information
-			mParticleCount = context.getState( State::Positions ).getPositions().size();
-			
-			mParticleMass.reserve( mParticleCount );
-			for( unsigned int i = 0; i < mParticleCount; i++ ){
-				mParticleMass.push_back( system.getParticleMass( i ) );
-			}
-			
-			// Create New System
-			System *blockSystem = new System();
-			cout << "res per block " << params.res_per_block << endl;
-			for( int i = 0; i < mParticleCount; i++ ) {
-				blockSystem->addParticle( mParticleMass[i] );
-			}
-            
-			int block_start = 0;
-			for( int i = 0; i < params.residue_sizes.size(); i++ ) {
-				if( i % params.res_per_block == 0 ) {
-					blocks.push_back( block_start );
-				}
-				block_start += params.residue_sizes[i];
-			}
-            
-			for( int i = 1; i < blocks.size(); i++ ) {
-				int block_size = blocks[i] - blocks[i - 1];
-				if( block_size > mLargestBlockSize ) {
-					mLargestBlockSize = block_size;
-				}
-			}
-            
-			mLargestBlockSize *= 3; // degrees of freedom in the largest block
-			cout << "blocks " << blocks.size() << endl;
-			cout << blocks[blocks.size() - 1] << endl;
-            
-			// Creating a whole new system called the blockSystem.
-			// This system will only contain bonds, angles, dihedrals, and impropers
-			// between atoms in the same block.
-			// Also contains pairwise force terms which are zeroed out for atoms
-			// in different blocks.
-			// This necessitates some copying from the original system, but is required
-			// because OpenMM populates all data when it reads XML.
-			// Copy all atoms into the block system.
-            
-			// Copy the center of mass force.
-			cout << "adding forces..." << endl;
-			for( int i = 0; i < params.forces.size(); i++ ) {
-				string forcename = params.forces[i].name;
-				cout << "Adding force " << forcename << " at index " << params.forces[i].index << endl;
-				if( forcename == "CenterOfMass" ) {
-					blockSystem->addForce( &system.getForce( params.forces[i].index ) );
-				} else if( forcename == "Bond" ) {
-					// Create a new harmonic bond force.
-					// This only contains pairs of atoms which are in the same block.
-					// I have to iterate through each bond from the old force, then
-					// selectively add them to the new force based on this condition.
-					HarmonicBondForce *hf = new HarmonicBondForce();
-					const HarmonicBondForce *ohf = dynamic_cast<const HarmonicBondForce *>( &system.getForce( params.forces[i].index ) );
-					for( int i = 0; i < ohf->getNumBonds(); i++ ) {
-						// For our system, add bonds between atoms in the same block
-						int particle1, particle2;
-						double length, k;
-						ohf->getBondParameters( i, particle1, particle2, length, k );
-						if( inSameBlock( particle1, particle2 ) ) {
-							hf->addBond( particle1, particle2, length, k );
-						}
-					}
-					blockSystem->addForce( hf );
-				} else if( forcename == "Angle" ) {
-					// Same thing with the angle force....
-					HarmonicAngleForce *af = new HarmonicAngleForce();
-					const HarmonicAngleForce *ahf = dynamic_cast<const HarmonicAngleForce *>( &system.getForce( params.forces[i].index ) );
-					for( int i = 0; i < ahf->getNumAngles(); i++ ) {
-						// For our system, add bonds between atoms in the same block
-						int particle1, particle2, particle3;
-						double angle, k;
-						ahf->getAngleParameters( i, particle1, particle2, particle3, angle, k );
-						if( inSameBlock( particle1, particle2, particle3 ) ) {
-							af->addAngle( particle1, particle2, particle3, angle, k );
-						}
-					}
-					blockSystem->addForce( af );
-				} else if( forcename == "Dihedral" ) {
-					// And the dihedrals....
-					PeriodicTorsionForce *ptf = new PeriodicTorsionForce();
-					const PeriodicTorsionForce *optf = dynamic_cast<const PeriodicTorsionForce *>( &system.getForce( params.forces[i].index ) );
-					for( int i = 0; i < optf->getNumTorsions(); i++ ) {
-						// For our system, add bonds between atoms in the same block
-						int particle1, particle2, particle3, particle4, periodicity;
-						double phase, k;
-						optf->getTorsionParameters( i, particle1, particle2, particle3, particle4, periodicity, phase, k );
-						if( inSameBlock( particle1, particle2, particle3, particle4 ) ) {
-							ptf->addTorsion( particle1, particle2, particle3, particle4, periodicity, phase, k );
-						}
-					}
-					blockSystem->addForce( ptf );
-				} else if( forcename == "Improper" ) {
-					// And the impropers....
-					RBTorsionForce *rbtf = new RBTorsionForce();
-					const RBTorsionForce *orbtf = dynamic_cast<const RBTorsionForce *>( &system.getForce( params.forces[i].index ) );
-					for( int i = 0; i < orbtf->getNumTorsions(); i++ ) {
-						// For our system, add bonds between atoms in the same block
-						int particle1, particle2, particle3, particle4;
-						double c0, c1, c2, c3, c4, c5;
-						orbtf->getTorsionParameters( i, particle1, particle2, particle3, particle4, c0, c1, c2, c3, c4, c5 );
-						if( inSameBlock( particle1, particle2, particle3, particle4 ) ) {
-							rbtf->addTorsion( particle1, particle2, particle3, particle4, c0, c1, c2, c3, c4, c5 );
-						}
-					}
-					blockSystem->addForce( rbtf );
-				} else if( forcename == "Nonbonded" ) {
-					// This is a custom nonbonded pairwise force and
-					// includes terms for both LJ and Coulomb.
-					// Note that the step term will go to zero if block1 does not equal block 2,
-					// and will be one otherwise.
-					CustomBondForce *cbf = new CustomBondForce( "4*eps*((sigma/r)^12-(sigma/r)^6)+138.935456*q/r" );
-					const NonbondedForce *nbf = dynamic_cast<const NonbondedForce *>( &system.getForce( params.forces[i].index ) );
-                    
-					cbf->addPerBondParameter( "q" );
-					cbf->addPerBondParameter( "sigma" );
-					cbf->addPerBondParameter( "eps" );
-                    
-					// store exceptions
-					// exceptions[p1][p2] = params
-					map<int, map<int, vector<double> > > exceptions;
-                    
-					for( int i = 0; i < nbf->getNumExceptions(); i++ ) {
-						int p1, p2;
-						double q, sig, eps;
-						nbf->getExceptionParameters( i, p1, p2, q, sig, eps );
-						if( inSameBlock( p1, p2 ) ) {
-							vector<double> params;
-							params.push_back( q );
-							params.push_back( sig );
-							params.push_back( eps );
-							if( exceptions.count( p1 ) == 0 ) {
-								map<int, vector<double> > pair_exception;
-								pair_exception[p2] = params;
-								exceptions[p1] = pair_exception;
-							} else {
-								exceptions[p1][p2] = params;
-							}
-						}
-					}
-                    
-					// add particle params
-					// TODO: iterate over block dimensions to reduce to O(b^2 N_b)
-					for( int i = 0; i < nbf->getNumParticles() - 1; i++ ) {
-						for( int j = i + 1; j < nbf->getNumParticles(); j++ ) {
-							if( !inSameBlock( i, j ) ) {
-								continue;
-							}
-							// we have an exception -- 1-4 modified interactions, etc.
-							if( exceptions.count( i ) == 1 && exceptions[i].count( j ) == 1 ) {
-								vector<double> params = exceptions[i][j];
-								cbf->addBond( i, j, params );
-							}
-							// no exception, normal interaction
-							else {
-								vector<double> params;
-								double q1, q2, eps1, eps2, sigma1, sigma2, q, eps, sigma;
-                                
-								nbf->getParticleParameters( i, q1, sigma1, eps1 );
-								nbf->getParticleParameters( j, q2, sigma2, eps2 );
-                                
-								q = q1 * q2;
-								sigma = 0.5 * ( sigma1 + sigma2 );
-								eps = sqrt( eps1 * eps2 );
-                                
-								params.push_back( q );
-								params.push_back( sigma );
-								params.push_back( eps );
-                                
-								cbf->addBond( i, j, params );
-							}
-						}
-					}
-                    
-					blockSystem->addForce( cbf );
-				} else {
-					cout << "Unknown Force: " << forcename << endl;
-				}
-			}
-			cout << "done." << endl;
-            
-			VerletIntegrator *integ = new VerletIntegrator( 0.000001 );
-			if( blockContext ) {
-				delete blockContext;
-			}
-			
-			switch( params.BlockDiagonalizePlatform ){
-				case Preference::Reference:{
-					blockContext = new Context( *blockSystem, *integ, Platform::getPlatformByName( "Reference" ) );
-					break;
-				}
-				case Preference::OpenCL:{
-					blockContext = new Context( *blockSystem, *integ, Platform::getPlatformByName( "OpenCL" ) );
-					break;
-				}
-				case Preference::CUDA:{
-					blockContext = new Context( *blockSystem, *integ, Platform::getPlatformByName( "Cuda" ) );
-					break;
-				}
-			}
-			
-            
-			mInitialized = true;
-            
-#ifdef PROFILE_ANALYSIS
-			gettimeofday( &end, 0 );
-			double elapsed = ( end.tv_sec - start.tv_sec ) * 1000.0 + ( end.tv_usec - start.tv_usec ) / 1000.0;
-			std::cout << "[Analysis] Initialize: " << elapsed << "ms" << std::endl;
 #endif
 		}
         
